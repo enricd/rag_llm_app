a6789569c19b500ca222fd0d5d56b33c51627443
enricd <edomingodomenech@gmail.com>
2024-09-15 21:08:46 +0200

    rag app completed

---
+++ app.py
@@ -0,0 +1,159 @@
+import streamlit as st
+import os
+import dotenv
+import uuid
+
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain.schema import HumanMessage, AIMessage
+
+from rag_methods import (
+    load_doc_to_db, 
+    load_url_to_db,
+    stream_llm_response,
+    stream_llm_rag_response,
+)
+
+dotenv.load_dotenv()
+
+MODELS = [
+    # "openai/o1-mini",
+    "openai/gpt-4o",
+    "openai/gpt-4o-mini",
+    "anthropic/claude-3-5-sonnet-20240620",
+]
+
+st.set_page_config(
+    page_title="RAG LLM app?", 
+    page_icon="📚", 
+    layout="centered", 
+    initial_sidebar_state="expanded"
+)
+
+
+# --- Header ---
+st.html("""<h2 style="text-align: center;">📚🔍 <i> Do your LLM even RAG bro? </i> 🤖💬</h2>""")
+
+
+# --- Initial Setup ---
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+
+if "rag_sources" not in st.session_state:
+    st.session_state.rag_sources = []
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{
+        "role": "assistant", 
+        "content": "Hi there! How can I assist you today?"
+    }]
+
+
+# --- Side Bar LLM API Tokens ---
+with st.sidebar:
+    default_openai_api_key = os.getenv("OPENAI_API_KEY") if os.getenv("OPENAI_API_KEY") is not None else ""  # only for development environment, otherwise it should return None
+    with st.popover("🔐 OpenAI"):
+        openai_api_key = st.text_input(
+            "Introduce your OpenAI API Key (https://platform.openai.com/)", 
+            value=default_openai_api_key, 
+            type="password",
+        )
+
+    default_anthropic_api_key = os.getenv("ANTHROPIC_API_KEY") if os.getenv("ANTHROPIC_API_KEY") is not None else ""
+    with st.popover("🔐 Anthropic"):
+        anthropic_api_key = st.text_input(
+            "Introduce your Anthropic API Key (https://console.anthropic.com/)", 
+            value=default_anthropic_api_key, 
+            type="password",
+        )
+
+# --- Main Content ---
+# Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
+missing_openai = openai_api_key == "" or openai_api_key is None or "sk-" not in openai_api_key
+missing_anthropic = anthropic_api_key == "" or anthropic_api_key is None
+if missing_openai and missing_anthropic:
+    st.write("#")
+    st.warning("⬅️ Please introduce an API Key to continue...")
+
+else:
+    # Sidebar
+    with st.sidebar:
+        st.divider()
+        st.selectbox(
+            "🤖 Select a Model", 
+            [model for model in MODELS if ("openai" in model and not missing_openai) or ("anthropic" in model and not missing_anthropic)],
+            key="model",
+        )
+
+        cols0 = st.columns(2)
+        with cols0[0]:
+            is_vector_db_loaded = ("vector_db" in st.session_state and st.session_state.vector_db is not None)
+            st.toggle(
+                "Use RAG", 
+                value=is_vector_db_loaded, 
+                key="use_rag", 
+                disabled=not is_vector_db_loaded,
+            )
+
+        with cols0[1]:
+            st.button("Clear Chat", on_click=lambda: st.session_state.messages.clear(), type="primary")
+
+        st.header("RAG Sources:")
+            
+        # File upload input for RAG with documents
+        st.file_uploader(
+            "📄 Upload a document", 
+            type=["pdf", "txt", "docx", "md"],
+            accept_multiple_files=True,
+            on_change=load_doc_to_db,
+            key="rag_docs",
+        )
+
+        # URL input for RAG with websites
+        st.text_input(
+            "🌐 Introduce a URL", 
+            placeholder="https://example.com",
+            on_change=load_url_to_db,
+            key="rag_url",
+        )
+
+        with st.expander(f"📚 Documents in DB ({0 if not is_vector_db_loaded else len(st.session_state.rag_sources)})"):
+            st.write([] if not is_vector_db_loaded else [source for source in st.session_state.rag_sources])
+
+    
+    # Main chat app
+    model_provider = st.session_state.model.split("/")[0]
+    if model_provider == "openai":
+        llm_stream = ChatOpenAI(
+            model_name=st.session_state.model.split("/")[-1],
+            temperature=0.3,
+            streaming=True,
+        )
+    elif model_provider == "anthropic":
+        llm_stream = ChatAnthropic(
+            model=st.session_state.model.split("/")[-1],
+            temperature=0.3,
+            streaming=True,
+        )
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Your message"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+
+            messages = [HumanMessage(content=m["content"]) if m["role"] == "user" else AIMessage(content=m["content"]) for m in st.session_state.messages]
+
+            if not st.session_state.use_rag:
+                st.write_stream(stream_llm_response(llm_stream, messages))
+            else:
+                st.write_stream(stream_llm_rag_response(llm_stream, messages))
+
+    
---
+++ rag_methods.py
@@ -0,0 +1,171 @@
+import os
+import dotenv
+from time import time
+import streamlit as st
+
+from langchain_community.document_loaders.text import TextLoader
+from langchain_community.document_loaders import (
+    WebBaseLoader, 
+    PyPDFLoader, 
+    Docx2txtLoader,
+)
+# pip install docx2txt, pypdf
+from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+dotenv.load_dotenv()
+
+os.environ["USER_AGENT"] = "myagent"
+DB_DOCS_LIMIT = 10
+
+# Function to stream the response of the LLM 
+def stream_llm_response(llm_stream, messages):
+    response_message = ""
+
+    for chunk in llm_stream.stream(messages):
+        response_message += chunk.content
+        yield chunk
+
+    st.session_state.messages.append({"role": "assistant", "content": response_message})
+
+
+# --- Indexing Phase ---
+
+def load_doc_to_db():
+    # Use loader according to doc type
+    if "rag_docs" in st.session_state and st.session_state.rag_docs:
+        docs = [] 
+        for doc_file in st.session_state.rag_docs:
+            if doc_file.name not in st.session_state.rag_sources:
+                if len(st.session_state.rag_sources) < DB_DOCS_LIMIT:
+                    os.makedirs("source_files", exist_ok=True)
+                    file_path = f"./source_files/{doc_file.name}"
+                    with open(file_path, "wb") as file:
+                        file.write(doc_file.read())
+
+                    try:
+                        if doc_file.type == "application/pdf":
+                            loader = PyPDFLoader(file_path)
+                        elif doc_file.name.endswith(".docx"):
+                            loader = Docx2txtLoader(file_path)
+                        elif doc_file.type in ["text/plain", "text/markdown"]:
+                            loader = TextLoader(file_path)
+                        else:
+                            st.warning(f"Document type {doc_file.type} not supported.")
+                            continue
+
+                        docs.extend(loader.load())
+                        st.session_state.rag_sources.append(doc_file.name)
+
+                    except Exception as e:
+                        st.toast(f"Error loading document {doc_file.name}: {e}", icon="⚠️")
+                        print(f"Error loading document {doc_file.name}: {e}")
+                    
+                    finally:
+                        os.remove(file_path)
+
+                else:
+                    st.error(F"Maximum number of documents reached ({DB_DOCS_LIMIT}).")
+
+        if docs:
+            _split_and_load_docs(docs)
+            st.toast(f"Document *{str([doc_file.name for doc_file in st.session_state.rag_docs])[1:-1]}* loaded successfully.", icon="✅")
+
+
+def load_url_to_db():
+    if "rag_url" in st.session_state and st.session_state.rag_url:
+        url = st.session_state.rag_url
+        docs = []
+        if url not in st.session_state.rag_sources:
+            if len(st.session_state.rag_sources) < 10:
+                try:
+                    loader = WebBaseLoader(url)
+                    docs.extend(loader.load())
+                    st.session_state.rag_sources.append(url)
+
+                except Exception as e:
+                    st.error(f"Error loading document from {url}: {e}")
+
+                if docs:
+                    _split_and_load_docs(docs)
+                    st.toast(f"Document from URL *{url}* loaded successfully.", icon="✅")
+
+            else:
+                st.error("Maximum number of documents reached (10).")
+
+
+def initialize_vector_db(docs):
+    vector_db = Chroma.from_documents(
+        documents=docs,
+        embedding=OpenAIEmbeddings(),
+        collection_name=f"{str(time()).replace('.', '')[:14]}_" + st.session_state['session_id'],
+    )
+
+    # We need to manage the number of collections that we have in memory, we will keep the last 20
+    chroma_client = vector_db._client
+    collection_names = sorted([collection.name for collection in chroma_client.list_collections()])
+    print("Number of collections:", len(collection_names))
+    while len(collection_names) > 20:
+        chroma_client.delete_collection(collection_names[0])
+        collection_names.pop(0)
+
+    return vector_db
+
+
+def _split_and_load_docs(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=5000,
+        chunk_overlap=1000,
+    )
+
+    document_chunks = text_splitter.split_documents(docs)
+
+    if "vector_db" not in st.session_state:
+        st.session_state.vector_db = initialize_vector_db(docs)
+    else:
+        st.session_state.vector_db.add_documents(document_chunks)
+
+
+# --- Retrieval Augmented Generation (RAG) Phase ---
+
+def _get_context_retriever_chain(vector_db, llm):
+    retriever = vector_db.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="messages"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get inforamtion relevant to the conversation, focusing on the most recent messages."),
+    ])
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+
+def get_conversational_rag_chain(llm):
+    retriever_chain = _get_context_retriever_chain(st.session_state.vector_db, llm)
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system",
+        """You are a helpful assistant. You will have to answer to user's queries.
+        You will have some context to help with your answers, but now always would be completely related or helpful.
+        You can also use your knowledge to assist answering the user's queries.\n
+        {context}"""),
+        MessagesPlaceholder(variable_name="messages"),
+        ("user", "{input}"),
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+
+def stream_llm_rag_response(llm_stream, messages):
+    conversation_rag_chain = get_conversational_rag_chain(llm_stream)
+    response_message = "*(RAG Response)*\n"
+    for chunk in conversation_rag_chain.pick("answer").stream({"messages": messages[:-1], "input": messages[-1].content}):
+        response_message += chunk
+        yield chunk
+
+    st.session_state.messages.append({"role": "assistant", "content": response_message})
